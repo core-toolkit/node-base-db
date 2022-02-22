993539f61e4cf379614079f02eb768b2a23465fa
tecs <ttecss@gmail.com>
2022-02-22 18:58:16 +0200

    Actually register cli commands on init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const CreateModel = require('./cli/commands/create-model');
 const MakeDbClient = require('./clients/DbClient');
 const MakeMigrator = require('./services/MigrationService');
 const ModelFactory = require('./factories/Model');
@@ -12,7 +13,22 @@ module.exports = (app) => {
   app.appendTypeParameters('Service', 'Model');
   app.appendTypeParameters('UseCase', 'Model');
 
-  const { start } = app;
+  const { start, initAll } = app;
+
+  app.initAll = async () => {
+    const { Core: { Cli } } = await app.resolveDependencies(['Core']);
+    if (!Cli.list().includes('create:model')) {
+      Cli.register({
+        name: 'create:model',
+        args: ['name'],
+        describe: 'Create new model',
+        exec: CreateModel,
+      });
+    }
+
+    return initAll();
+  };
+
   app.start = async () => {
     await start();
     const { Service } = await app.resolveDependencies(['Service']);
