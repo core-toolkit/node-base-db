5ca234d6414e0c1f9645c2ff52b9a53dedd79e14
tecs <ttecss@gmail.com>
2022-02-28 22:04:17 +0200

    Revised CLI interface's usage

--- src/cli/commands/init.js
+++ src/cli/commands/init.js
@@ -1,11 +1,11 @@
 const supportedDialects = ['postgres'];
 
-module.exports = ({ dialect }, { addToConfig, addPackage, addAppToRoot }) => {
+module.exports = ({ dialect }, { addToConfig, addBasePackage, addAppToRoot }) => {
   if (!supportedDialects.includes(dialect)) {
     throw new Error(`Unsupported dialect ${dialect}, must be one of: [${supportedDialects.join(', ')}]`);
   }
 
   addToConfig('db:config.js', { dialect, dialectUpper: dialect.toUpperCase() });
-  addPackage(`node-base-db-${dialect}`);
+  addBasePackage(`node-base-db-${dialect}`);
   addAppToRoot('Db');
 };
