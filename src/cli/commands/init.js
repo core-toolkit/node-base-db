537fead49f9a0d054fcfb02fc0e2114aa0c90e7f
tecs <ttecss@gmail.com>
2022-03-07 19:15:34 +0200

    Converted commands to the new node-base interface

--- src/cli/commands/init.js
+++ src/cli/commands/init.js
@@ -1,11 +1,11 @@
 const supportedDialects = ['postgres'];
 
-module.exports = ({ dialect }, { addToConfig, addBasePackage, addAppToRoot }) => {
+module.exports = async ({ dialect }, { addToConfig, addBasePackage, addAppToRoot }) => {
   if (!supportedDialects.includes(dialect)) {
     throw new Error(`Unsupported dialect ${dialect}, must be one of: [${supportedDialects.join(', ')}]`);
   }
 
   addToConfig('db:config.js', { dialect, dialectUpper: dialect.toUpperCase() });
-  addBasePackage(`node-base-db-${dialect}`);
+  await addBasePackage(`db-${dialect}`);
   addAppToRoot('Db');
 };
